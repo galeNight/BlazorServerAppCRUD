399e54fe9123fbeb6881997a8d4a2787c450f6e6
Jannick Mørup <jannickmorup@gmail.com>
2023-04-28 10:12:51 +0200

    added to buttons to index.razor

--- BlazorServerAppCRUD/Models/UserEntity.cs
+++ BlazorServerAppCRUD/Models/UserEntity.cs
@@ -10,7 +10,6 @@ namespace BlazorServerAppCRUD.Models
         [Required(ErrorMessage = "First Name is required")]
         public string? FirstName { get; set; }
 
-
         [Required(ErrorMessage = "Last Name is required")]
         public string? LastName { get; set; }
 
